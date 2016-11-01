33d155fb420365babd8cf9ecf7f0e01b87572051
Alexey Sokolov <sokol789@yandex.ru>
2016-11-01 18:55:10 +0300

    init

--- q7.js
+++ q7.js
@@ -1,3 +1,5 @@
+var expect = require("chai").expect;
+
 const curried = curry((a, b, c) => a + b + c);
 
 curried(1, 2, 3);  // 6
@@ -6,3 +8,7 @@ curried(1, 2)(3);  // 6
 
 function curry(origFunc) {
 }
+
+expect(curried(1, 2, 3)).to.equal(6);
+expect(curried(1)(2)(3)).to.equal(6);
+expect(curried(1, 2)(3)).to.equal(6);
