aba0ecbc4d70958367d63c7acf40b846b304e974
Alexey Sokolov <sokol789@yandex.ru>
2017-07-27 18:59:57 +0300

    pg

--- q10.js
+++ q10.js
@@ -2,4 +2,9 @@ const data = [1,2,4,7,11,14,15,17,22,25,29,31,32];
 
 const bSearch = (num, data) => {};
 
-bSearch(11, data);
+console.log(bSearch(11, data)); // 4
+console.log(bSearch(1, data)); // 0
+console.log(bSearch(45, data)); // -1
+console.log(bSearch(31, data)); // 11
+console.log(bSearch(32, data)); // 12
+console.log(bSearch(0, data)); // -1
